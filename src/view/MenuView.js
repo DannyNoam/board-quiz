e39917b306915a5d2a2a50f465b45e8011bbaa79
Danny Noam <danny.noam@gamesys.co.uk>
2016-01-05 16:07:22 +0000

    Amended MenuView to render using layout data.

--- src/view/MenuView.js
+++ src/view/MenuView.js
@@ -11,24 +11,24 @@ function MenuView() {
 MenuView.prototype.setupViewElements = function() {
     var layoutData = PIXI.Container.layoutData;
     this.boardQuizText = new PIXI.Text(layoutData.BOARD_QUIZ.text, {font: layoutData.BOARD_QUIZ.size + "px " + layoutData.BOARD_QUIZ.font, fill: layoutData.BOARD_QUIZ.color});
-    this.boardQuizText.position.x = 400;
-    this.boardQuizText.position.y = 100;
+    this.boardQuizText.position.x = layoutData.BOARD_QUIZ.x;
+    this.boardQuizText.position.y = layoutData.BOARD_QUIZ.y;
     this.boardQuizText.interactive = true;
     this.boardQuizText.anchor.x = 0.5;
     this.boardQuizText.anchor.y = 0.5;
     this.addChild(this.boardQuizText);
     
-    this.playButton = new PIXI.Sprite.fromImage('assets/play-button.gif');
-    this.playButton.position.x = 400;
-    this.playButton.position.y = 200;
+    this.playButton = new PIXI.Sprite.fromImage(layoutData.PLAY_BUTTON.path);
+    this.playButton.position.x = layoutData.PLAY_BUTTON.x;
+    this.playButton.position.y = layoutData.PLAY_BUTTON.x;
     this.playButton.anchor.x = 0.5;
     this.playButton.anchor.y = 0.5;
     this.playButton.interactive = true;
     this.addChild(this.playButton);
     
-    this.helpButton = new PIXI.Sprite.fromImage('assets/help.gif');
-    this.helpButton.position.x = 400;
-    this.helpButton.position.y = 420;
+    this.helpButton = new PIXI.Sprite.fromImage(layoutData.HELP_BUTTON.path);
+    this.helpButton.position.x = layoutData.HELP_BUTTON.x;
+    this.helpButton.position.y = layoutData.HELP_BUTTON.x;
     this.helpButton.anchor.x = 0.5;
     this.helpButton.anchor.y = 0.5;
     this.helpButton.interactive = true;
