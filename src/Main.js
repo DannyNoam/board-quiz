58c779f4b314655e3d5a3d852768c62a2fc7c813
Danny Noam <danny.noam@gamesys.co.uk>
2016-01-23 15:00:07 +0000

    Amendments.

--- src/Main.js
+++ src/Main.js
@@ -34,7 +34,7 @@ window.onload = function() {
     var DIV_ID = "game";
     
     (function() {
-        console.log("Initiated bucket loader.")
+        console.log("Initiated bucket loader.");
         new BucketLoader(loadLayout, bucketLoadingFailedMessage);
     })();
     
