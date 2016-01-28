412dec6b1969a0763abbdac1f2d4ade7cab6b9e6
Danny Noam <danny.noam@gamesys.co.uk>
2016-01-28 08:23:35 +0000

    Small tidy of server.js

--- server.js
+++ server.js
@@ -52,117 +52,117 @@ var setupServer = (function() {
 
 io.on(SocketConstants.on.CONNECTION, function (socket) {
     
-      socket.on(SocketConstants.on.FINDING_GAME, function (playerData) {
-          socket.playing = true;
-          players[PLAYER_1] = new Player(socket.id, playerData.avatar);
-          if(Lobby.getNumberOfPlayersWaiting() > 0) {
-              this.roomName = Lobby.getFirstRoom().name;
-              players[PLAYER_2] = Lobby.getFirstRoom().player;
-              joinRoom(this.roomName);
-              cleanUpLobbyForGameFound();
-              initiateGame(this.roomName, players[PLAYER_1], players[PLAYER_2]);
-              isGameFull = true;
-          } else {
-              joinRoom(this.roomName);
-              Lobby.addRoom(this.roomName, players[PLAYER_1]);
-              Lobby.addPlayer(players[PLAYER_1]);
-          }
-      }.bind(this));
+    socket.on(SocketConstants.on.FINDING_GAME, function (playerData) {
+      socket.playing = true;
+      players[PLAYER_1] = new Player(socket.id, playerData.avatar);
+      if(Lobby.getNumberOfPlayersWaiting() > 0) {
+          this.roomName = Lobby.getFirstRoom().name;
+          players[PLAYER_2] = Lobby.getFirstRoom().player;
+          joinRoom(this.roomName);
+          cleanUpLobbyForGameFound();
+          initiateGame(this.roomName, players[PLAYER_1], players[PLAYER_2]);
+          isGameFull = true;
+      } else {
+          joinRoom(this.roomName);
+          Lobby.addRoom(this.roomName, players[PLAYER_1]);
+          Lobby.addPlayer(players[PLAYER_1]);
+      }
+    }.bind(this));
 
-      socket.on(SocketConstants.on.GET_PLAYERS_HEALTH, function() {
-          socket.emit(SocketConstants.emit.PLAYERS_HEALTH, {player1Health: players[PLAYER_1].getHealth(), player2Health: players[PLAYER_2].getHealth()});
-      }.bind(this));
+    socket.on(SocketConstants.on.GET_PLAYERS_HEALTH, function() {
+      socket.emit(SocketConstants.emit.PLAYERS_HEALTH, {player1Health: players[PLAYER_1].getHealth(), player2Health: players[PLAYER_2].getHealth()});
+    }.bind(this));
 
-      socket.on(SocketConstants.on.DISCONNECT, function (playerData) {
-          Lobby.removePlayer(socket.id);
-          Lobby.removeRoom(socket.id);
-          if(socket.playing) {
-              isGameFull = false;
-          }
-      });
-
-      socket.on(SocketConstants.on.ROLL_DICE, function() {
-          io.to(this.roomName).emit(SocketConstants.emit.DICE_NUMBER, {number: Dice.roll()});
-      });
-
-        socket.on(SocketConstants.on.IS_GAME_FULL, function() {
-            if(!socket.playing) {
-                socket.emit(SocketConstants.emit.GAME_STATUS, isGameFull);
-            } else {
-                socket.emit(SocketConstants.emit.GAME_STATUS, false);
-            }
-        });
-
-      socket.on(SocketConstants.on.GET_RANDOM_QUESTION, function(data) {
-          var questionPrefix = 'QUESTION_';
-          var category = data.categories[Dice.roll()];
-          var question = data.questions[category][questionPrefix + randomNumberBetween(1, NUMBER_OF_QUESTIONS_PER_CATEGORY)];
-         io.to(this.roomName).emit(SocketConstants.emit.RANDOM_QUESTION, {category: category, question: question});
-      }.bind(this));
-
-      socket.on(SocketConstants.on.NEW_TURN, function() {
-          io.to(this.roomName).emit(SocketConstants.emit.INIT_NEW_TURN, {player1Health: players[PLAYER_1].getHealth(), player2Health: players[PLAYER_2].getHealth()});
-      }.bind(this));
-    
-    socket.on(SocketConstants.on.SET_SOCKET_PLAYING, function() {
-        socket.playing = true;
+    socket.on(SocketConstants.on.DISCONNECT, function (playerData) {
+      Lobby.removePlayer(socket.id);
+      Lobby.removeRoom(socket.id);
+      if(socket.playing) {
+          isGameFull = false;
+      }
     });
 
-      socket.on(SocketConstants.on.DEAL_DAMAGE, function(data) {
-          var playerToDamage = players[data.player_to_damage];
-          var playerWhoAnswered = players[data.player_who_answered];
-          updatePlayerAnswerStats(playerWhoAnswered, data);
-          playerToDamage.takeHit(data.damage);
-          io.to(this.roomName).emit(SocketConstants.emit.DAMAGE_DEALT, {player_who_answered: data.player_who_answered, player1Health: players[PLAYER_1].getHealth(), player2Health: players[PLAYER_2].getHealth(), answer: data.answer});
-      }.bind(this));
+    socket.on(SocketConstants.on.ROLL_DICE, function() {
+      io.to(this.roomName).emit(SocketConstants.emit.DICE_NUMBER, {number: Dice.roll()});
+    });
 
-      socket.on(SocketConstants.on.SHUFFLE_ANSWER_INDICES, function(data) {
-          var indices = data.indices.sort(function() {
-              return 0.5 - Math.random();
-          });
-          io.to(this.roomName).emit(SocketConstants.emit.SHUFFLED_ANSWER_INDICES, data.indices); 
+    socket.on(SocketConstants.on.IS_GAME_FULL, function() {
+        if(!socket.playing) {
+            socket.emit(SocketConstants.emit.GAME_STATUS, isGameFull);
+        } else {
+            socket.emit(SocketConstants.emit.GAME_STATUS, false);
+        }
+    });
+
+    socket.on(SocketConstants.on.GET_RANDOM_QUESTION, function(data) {
+      var questionPrefix = 'QUESTION_';
+      var category = data.categories[Dice.roll()];
+      var question = data.questions[category][questionPrefix + randomNumberBetween(1, NUMBER_OF_QUESTIONS_PER_CATEGORY)];
+     io.to(this.roomName).emit(SocketConstants.emit.RANDOM_QUESTION, {category: category, question: question});
+    }.bind(this));
+
+    socket.on(SocketConstants.on.NEW_TURN, function() {
+      io.to(this.roomName).emit(SocketConstants.emit.INIT_NEW_TURN, {player1Health: players[PLAYER_1].getHealth(), player2Health: players[PLAYER_2].getHealth()});
+    }.bind(this));
+
+    socket.on(SocketConstants.on.SET_SOCKET_PLAYING, function() {
+    socket.playing = true;
+    });
+
+    socket.on(SocketConstants.on.DEAL_DAMAGE, function(data) {
+      var playerToDamage = players[data.player_to_damage];
+      var playerWhoAnswered = players[data.player_who_answered];
+      updatePlayerAnswerStats(playerWhoAnswered, data);
+      playerToDamage.takeHit(data.damage);
+      io.to(this.roomName).emit(SocketConstants.emit.DAMAGE_DEALT, {player_who_answered: data.player_who_answered, player1Health: players[PLAYER_1].getHealth(), player2Health: players[PLAYER_2].getHealth(), answer: data.answer});
+    }.bind(this));
+
+    socket.on(SocketConstants.on.SHUFFLE_ANSWER_INDICES, function(data) {
+      var indices = data.indices.sort(function() {
+          return 0.5 - Math.random();
       });
+      io.to(this.roomName).emit(SocketConstants.emit.SHUFFLED_ANSWER_INDICES, data.indices); 
+    });
 
     socket.on(SocketConstants.on.GAME_ENDED, function(data) {
-        io.to(this.roomName).emit(SocketConstants.emit.GAME_STATS, {player1CorrectAnswerPercentage: players[PLAYER_1].calculateCorrectAnswerPercentage(), player1BestCategory: players[PLAYER_1].calculateBestCategory().name, player1BestCategoryPercentage: players[PLAYER_1].calculateBestCategory().percentageCorrect, player2CorrectAnswerPercentage: players[PLAYER_2].calculateCorrectAnswerPercentage(), player2BestCategory: players[PLAYER_2].calculateBestCategory().name, player2BestCategoryPercentage: players[PLAYER_2].calculateBestCategory().percentageCorrect, winner: data.winner});
+    io.to(this.roomName).emit(SocketConstants.emit.GAME_STATS, {player1CorrectAnswerPercentage: players[PLAYER_1].calculateCorrectAnswerPercentage(), player1BestCategory: players[PLAYER_1].calculateBestCategory().name, player1BestCategoryPercentage: players[PLAYER_1].calculateBestCategory().percentageCorrect, player2CorrectAnswerPercentage: players[PLAYER_2].calculateCorrectAnswerPercentage(), player2BestCategory: players[PLAYER_2].calculateBestCategory().name, player2BestCategoryPercentage: players[PLAYER_2].calculateBestCategory().percentageCorrect, winner: data.winner});
     }.bind(this));
 
 
-      function cleanUpLobbyForGameFound() {
-          Lobby.removePlayer(socket.id);
-          Lobby.removePlayer(Lobby.getFirstRoom().player.getId());
-          Lobby.removeRoom(Lobby.getFirstRoom().name);
-      }
+    function cleanUpLobbyForGameFound() {
+      Lobby.removePlayer(socket.id);
+      Lobby.removePlayer(Lobby.getFirstRoom().player.getId());
+      Lobby.removeRoom(Lobby.getFirstRoom().name);
+    }
 
-      function initiateGame(roomName, player1, player2) {
-          var player1Id = player1.getId();
-          var player2Id = player2.getId();
-          var player1Avatar = player1.getAvatar();
-          var player2Avatar = player2.getAvatar();
-          io.to(roomName).emit(SocketConstants.emit.GAME_FOUND, {roomName: roomName, player1Id: player1Id, player2Id: player2Id, player1Avatar: player1Avatar, player2Avatar: player2Avatar});
-          io.to(roomName).emit(SocketConstants.emit.SET_SOCKET_PLAYING);
-      }
+    function initiateGame(roomName, player1, player2) {
+      var player1Id = player1.getId();
+      var player2Id = player2.getId();
+      var player1Avatar = player1.getAvatar();
+      var player2Avatar = player2.getAvatar();
+      io.to(roomName).emit(SocketConstants.emit.GAME_FOUND, {roomName: roomName, player1Id: player1Id, player2Id: player2Id, player1Avatar: player1Avatar, player2Avatar: player2Avatar});
+      io.to(roomName).emit(SocketConstants.emit.SET_SOCKET_PLAYING);
+    }
 
-      function joinRoom(roomName) {
-          socket.join(roomName);
-      }
+    function joinRoom(roomName) {
+      socket.join(roomName);
+    }
 
-      function generateRoomName() {
-          var roomNamePrefix = 'Room_';
-          var roomName = roomNamePrefix + roomNumberSuffix;
-          roomNumberSuffix++;
-          return roomName;
-      }
+    function generateRoomName() {
+      var roomNamePrefix = 'Room_';
+      var roomName = roomNamePrefix + roomNumberSuffix;
+      roomNumberSuffix++;
+      return roomName;
+    }
 
-      function updatePlayerAnswerStats(player, data) {
-          if(data.answerStatus === 'correct') {
-              player.increaseCorrectAnswers();
-              player.increaseCategoryRightAnswer(data.category);
-          } else {
-              player.increaseWrongAnswers();
-              player.increaseCategoryWrongAnswer(data.category);
-          }
+    function updatePlayerAnswerStats(player, data) {
+      if(data.answerStatus === 'correct') {
+          player.increaseCorrectAnswers();
+          player.increaseCategoryRightAnswer(data.category);
+      } else {
+          player.increaseWrongAnswers();
+          player.increaseCategoryWrongAnswer(data.category);
       }
+    }
 });
 
 var Player = function(id, avatar) {
@@ -226,13 +226,10 @@ var Player = function(id, avatar) {
     };
     
     this.increaseCategoryRightAnswer = function(category) {
-        console.log(categoryAnswerData);
-        console.log(category);
         categoryAnswerData['' + category].right++;
     };
     
     this.increaseCategoryWrongAnswer = function(category) {
-        console.log("Category is " + category);
         categoryAnswerData[category].wrong++;
     };
     
@@ -253,8 +250,6 @@ var Player = function(id, avatar) {
             var numberOfWrongAnswers = categoryAnswerData[categories[i]].wrong;
             var totalAnswers = numberOfRightAnswers + numberOfWrongAnswers;
             var correctAnswerPercentage = numberOfRightAnswers > 0 ? ((numberOfRightAnswers/totalAnswers)*100).toFixed(0) : 0;
-            console.log("Category: " + categories[i]);
-            console.log("Percentage correct: " + correctAnswerPercentage);
             if(correctAnswerPercentage > (bestCategory.percentageCorrect)) {
                 bestCategory.name = categories[i];
                 bestCategory.percentageCorrect = correctAnswerPercentage;
